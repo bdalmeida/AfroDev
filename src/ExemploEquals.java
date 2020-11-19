4fe90a8e81f3612c664fdf48ca041b23f1271c49
Bruno Almeida <bdalmeida@live.com>
2020-11-18 21:08:06 -0300

    Exemplo Equals

---
+++ src/ExemploEquals.java
@@ -0,0 +1,12 @@
+public class ExemploEquals {
+    public static void main(String[] args) {
+        String nome1 = "Bruno";
+        String nome2 = "Bruno";
+
+        if (nome1.equals(nome2)){
+            System.out.println("Os nomes são iguais.");
+        }else{
+            System.out.println("Nomes diferentes.");
+        }
+    }
+}
